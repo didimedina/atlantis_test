cd9fcfcf03307a1f844a5a00fd0f06783e0c47a4
Didi Medina <hi@didimedina.com>
2020-02-09 10:39:56 +0200

    Update outputs.tf
    updated to example_didi

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
 output "public-ip" {
-  value = aws_instance.example.public_ip
+  value = aws_instance.example-didi.public_ip
 }
 
 output "instance-id" {
-  value = aws_instance.example.id
+  value = aws_instance.example-didi.id
 }
